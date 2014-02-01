45385c1bc672403dfe98aec843f3a095bbdc4645
swinghu <ogrecpp@163.com>
2014-02-01 23:15:13 +0800

    change the repo address

--- js/post.js
+++ js/post.js
@@ -51,7 +51,7 @@ $(document).ready(function(){
         var href = $(this).attr('href');
         if(href){
             if(href.indexOf('#') == 0){
-            }else if ( href.indexOf('/') == 0 || href.toLowerCase().indexOf('swinghu.github.io')>-1 || href.toLowerCase().indexOf('bareet.cc')>-1 ){
+            }else if ( href.indexOf('/') == 0 || href.toLowerCase().indexOf('swinghu.github.com')>-1 || href.toLowerCase().indexOf('bareet.cc')>-1 ){
             }else if ($(element).has('img').length){
             }else{
                 $(this).attr('target','_blank');
